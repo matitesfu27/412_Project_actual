5b3e7797af1af2393b31d1d4e5b08a22ecf09a72
edobbin <ethandobbin37@gmail.com>
2023-11-10 08:29:21 -0500

    reset

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebase=require('firebase')
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAS649QFsuYGyOxcblA0bLM3O-MTobxPPk",
+    authDomain: "cosc-412-c4189.firebaseapp.com",
+    databaseURL: "https://cosc-412-c4189-default-rtdb.firebaseio.com",
+    projectId: "cosc-412-c4189",
+    storageBucket: "cosc-412-c4189.appspot.com",
+    messagingSenderId: "446989331804",
+    appId: "1:446989331804:web:96a73809e081fb72546599",
+    measurementId: "G-WWEQEW5RQW"
+  };
